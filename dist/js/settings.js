129a5f5adeedbcd10233afb1eb2c0efaec13a9da
travis <travis@precise32.(none)>
2012-12-06 16:41:27 +0000

    Travis build 3534427 pushed to gh-pages

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -3,7 +3,7 @@
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.com/wet-boew/License-eng.txt / wet-boew.github.com/wet-boew/Licence-fra.txt
  *
- * Version: v3.1.0-b1 Build: 2012-12-06 02:39 PM
+ * Version: v3.1.0-b1 Build: 2012-12-06 04:40 PM
  *
  */
 /*
